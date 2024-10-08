c7dca7ee9d5b54b3d1a1b01daccaed3feafc193b
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-10-09 10:46:28 +1100

    Update home-route.tsx

---
+++ src/app/routes/app/home-route.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { homeConst as CONST } from '@/app/constants';
+import { homeAssets as ASSETS } from '@/assets';
+
+type HomeRouteProps = {
+    pageRef: React.Ref<any>;
+    navigateTo: (pageRef: string) => void;
+}
+
+export const HomeRoute: React.FC<HomeRouteProps> = ({pageRef, navigateTo}) => {
+    return (
+      <section 
+        ref={pageRef} 
+        className={``}
+      >
+        <div className={`flex flex-row w-full h-full`}>
+          <div className={`flex-1`}>
+          
+          </div>
+          
+          <div className={`flex-1`}>
+            <div className={`flex flex-column`}>
+              <h2>{CONST.title}</h2>
+              <p>{CONST.paragraph}</p>
+              <button>{CONST.button}</button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+}
