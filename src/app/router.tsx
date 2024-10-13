30e8c7e065a5f67214c5453312e51bf3c5e51721
xbrownx <asbrownn@gmail.com>
2024-10-13 16:10:30 +1100

    pc push

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,1 +1,1 @@
-import { AppRoot } from '@/app/routes/app/root';import { LoadingScreen } from '@/app/routes/loading';export const AppRouter = () => {    return (        <>            {/*<LoadingScreen />*/}            <AppRoot />        </>    );};
+import { AppRoot } from '@/app/routes/app/root';import { LoadingScreen } from '@/app/routes/loading';export const AppRouter = () => {    return (        <>            <LoadingScreen />            <AppRoot />        </>    );};
